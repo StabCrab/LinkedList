fcf8259a20777feb18444d4ad1b38e5d7989c605
StabCrab <37272657+StabCrab@users.noreply.github.com>
2020-05-30 11:31:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include "LinkedList.h"
 int main()
 {
-    auto *list = new LinkedList<int>;
-    list->pushBack(4);
-    list->pushBack(32);
-    list->pushFront(1);
-    list->pushBack(2);
-    list->removeBack();
+    LinkedList<int> list;
+    list.pushBack(4);
+    list.pushBack(32);
+    list.pushFront(1);
+    list.pushBack(2);
+    list.removeBack();
 //   list->print();
-    list->removeFront();
-    list->pushBack(1);
-    list->pushBack(2);
-    list->pushBack(3);
-    list->pushBack(4);
-    list->print();
-    list->reverse();
-    list->print();
+    list.removeFront();
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushBack(3);
+    list.pushBack(4);
+    list.print();
+    list.reverse();
+    list.print();
 //    list->print();
 }
